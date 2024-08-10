bdf2d46d5e3f232a0501881c04baf1b59dd8c314
franz1us <franzius.won@gmail.com>
2024-08-10 13:21:19 +0700

    update navBar
    update navBar

--- UserManagement/src/components/Header.jsx
+++ UserManagement/src/components/Header.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import '../css/Header.css';
-import brand from '../assets/brand.jpg'
+import brand from '../assets/brand.jpg';
 
 function Header() {
   return (
     <div className="header-container">
       <div className="header">
-        <ul>
-          <li><a href="#"><img src={brand} alt="brand" width="70px" /></a></li>
-          <li ><a className='header-list' href="#">Home</a></li>
-          <li ><a className='header-list' href="#">About</a></li>
-          <li ><a className='header-list' href="#">Contact</a></li>
-        </ul>
+        <a href="#" className="brand-logo">
+          <img src={brand} alt="brand" />
+        </a>
+        <input type="checkbox" id="nav-toggle" className="nav-toggle" />
+        <nav>
+          <ul className="header-list-container">
+            <li><a className="header-list" href="#">Home</a></li>
+            <li><a className="header-list" href="#">About</a></li>
+            <li><a className="header-list" href="#">Contact</a></li>
+            <li className="login-register">
+              <a href="#">Login</a>
+              <span> / </span>
+              <a href="#">Register</a>
+            </li>
+          </ul>
+        </nav>
+        <label htmlFor="nav-toggle" className="nav-toggle-label">
+          <span></span>
+        </label>
       </div>
     </div>
   );
